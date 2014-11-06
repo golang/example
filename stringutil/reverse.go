ec2c99bcdde76235a0ed4e2b7824b1727e2acc6e
Andrew Gerrand <adg@golang.org>
2014-11-07 10:10:44 +1100

    Update Reverse comment
    Change-Id: I6e435391fc5d1d67af96ea88dcce3ccdd204d146

--- stringutil/reverse.go
+++ stringutil/reverse.go
@@ -17,7 +17,7 @@ limitations under the License.
 // Package stringutil contains utility functions for working with strings.
 package stringutil
 
-// Reverse returns the provided string with its runes in reverse order.
+// Reverse returns its argument string reversed rune-wise left to right.
 func Reverse(s string) string {
 	r := []rune(s)
 	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
