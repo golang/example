94d5a47c652b6e694c89de89ec8ef78b3a09ab06
shivmishra88 <shiv@ebctechnologies.com>
2018-07-03 22:49:56 +0530

    Update main.go

--- outyet/main.go
+++ outyet/main.go
@@ -31,7 +31,7 @@ import (
 
 // Command-line flags.
 var (
-	httpAddr   = flag.String("http", ":8080", "Listen address")
+	httpAddr   = flag.String("http", ":8082", "Listen address")
 	pollPeriod = flag.Duration("poll", 5*time.Second, "Poll period")
 	version    = flag.String("version", "1.4", "Go version")
 )
