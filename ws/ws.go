c0cdcbd41600e82e27c7c4455e8ae5ec95823e19
John Marx <42839573+johnmarx-la@users.noreply.github.com>
2021-08-02 10:03:54 -0400

    Create ws.go

---
+++ ws/ws.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+)
+
+func main() {
+    http.HandleFunc("/", HelloServer)
+    http.ListenAndServe(":8080", nil)
+}
+
+func HelloServer(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+}
