9f3e1052b97a56b08daaa418ef9400988c21b8f9
Richard Knop <richard.knop@esgbook.com>
2025-08-14 20:54:29 +0100

    ragserver: update weaviate library to v1.32.3 and client to v5

--- ragserver/ragserver/weaviate.go
+++ ragserver/ragserver/weaviate.go
@@ -12,7 +12,7 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/weaviate/weaviate-go-client/v4/weaviate"
+	"github.com/weaviate/weaviate-go-client/v5/weaviate"
 	"github.com/weaviate/weaviate/entities/models"
 )
 
